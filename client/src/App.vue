a1988edcad80cc644bffe1a4866c9e092b9cb5af
= <=>
2022-07-29 14:49:35 +0700

    setup routerview

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <nav-bar />
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import NavBar from '@/components/NavBar.vue'
+    export default {
+        components: {
+          NavBar
+        }
+    }
 </script>
 
 <style>
@@ -21,6 +22,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
