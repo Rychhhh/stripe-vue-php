4f31d344690f52d1132d7e591d52be3ec26808b7
= <=>
2022-07-29 14:54:57 +0700

    some

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,29 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+ module.exports = {
+  publicPath: process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? '/admin-one-vue-tailwind/'
+    : '/',
+
+  // Remove moment.js from chart.js
+  configureWebpack: config => {
+    return {
+      externals: {
+        moment: 'moment'
+      }
+    }
+  },
+  runtimeCompiler: true,
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'id',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
+  }
+}
